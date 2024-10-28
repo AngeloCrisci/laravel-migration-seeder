d6c31607976f1125db4b1de3db68019b26432b3e
AngeloCrisci <angelocrisci1998@gmail.com>
2024-10-28 15:44:02 +0100

    Created controller model and populated the table of trains

--- database/migrations/2024_10_28_135952_create_trains_table.php
+++ database/migrations/2024_10_28_135952_create_trains_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string("company")->nullable(false);
+            $table->string("station_departure")->nullable(false);
+            $table->string("station_arrival")->nullable(false);
+            $table->time("time_of_the_departure")->nullable(false);
+            $table->date("day_of_the_departure")->nullable(false);
+            $table->time("time_of_the_arrival")->nullable(false);
+            $table->date("day_of_the_arrival")->nullable(false);
+            $table->string("train_code" , 15)->unique()->nullable(false);
+            $table->tinyInteger("wagons")->unsigned()->nullable(false);
+            $table->boolean("on_time")->default(true)->nullable(false);
+            $table->boolean("suspended")->default(false)->nullable(false);
             $table->timestamps();
         });
     }
